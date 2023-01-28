6e7d11d27df2ec4461b2ed6c3809e9b895c80662
Biks Yuriy <112703000+BiksYuriy@users.noreply.github.com>
2023-01-28 13:58:20 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -4,45 +4,100 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import numpy as np
-
-
 from openpyxl import Workbook, load_workbook
-
 import warnings
 warnings.filterwarnings('ignore', category=UserWarning, module='openpyxl')
 
-wb = load_workbook('Beetroot_Final_project.xlsx')
+wb = load_workbook('Beetroot_Final_project.xlsx',data_only=True)
 # Завантажуємо дані у сторінку вихідних даних
-ws = wb['Расчеты по Коршунову']
+ws = wb['Свод_таблица']
+row_count = ws.max_row
 
-# for r in dataframe_to_rows(df, index=False, header=False):
-#     ws1.append(r)
-
-for cell in ws['C31:C35']:
-    for value in np.linspace(0.2,0.5,num = 6):
-        ws.append(value)
-
-
-#from documentation openpyxl
- # https: // openpyxl.readthedocs.io / en / stable / _modules / openpyxl / worksheet / worksheet.html
-# Worksheet.insert_rows
-
-#row_idx = self._current_row + 1
-def insert_rows(self, idx, amount=1):
-            """
-            Insert row or rows before row==idx
-            """
-    #self._move_cells(min_row=idx, offset=amount, row_or_col="row")
-    #self._current_row = self.max_row
+# Вихідні дані для підстановки у комірки:
+a_range = np.linspace(0.02, 0.1, num=10)
+b_range = np.linspace(0.1, 0.3, num=10)
+c_range = np.linspace(0.1, 0.2, num=5)
+d_range = np.linspace(0.1, 0.4, num=10)
+e_range = np.linspace(0.02, 0.1, num=10)
 
 
-#ws['C31'] = 0.2
-#ws['C32'] = 0.1
-#ws['C33'] = 0.2
-#ws['C34'] = 0.15
-#ws['C35'] = 0.2
+data = [ws.cell(row=26, column=n).value for n in range(3, 25)]
+for row in range(27, row_count+1):
+    for i in a_range:
+        ws.cell(row=26, column=3).value = i
+    for j in b_range:
+        ws.cell(row=26, column=4).value = j
+    for k in c_range:
+        ws.cell(row=26, column=5).value = k
+    for l in d_range:
+        ws.cell(row=26, column=6).value = l
+    for m in e_range:
+        ws.cell(row=26, column=7).value = m
+    for col in range(3, 25):
+        ws.cell(row=row, column=col).value = ws.cell(row=26, column=col).value
+wb.save('Beetroot_Final_project.test.xlsx')
+# # Підстановка даних у комірки С26:G26
+# # for i in range (5):
+# for i,j in enumerate(np.linspace(0.2, 0.5, num=5)):
+#     print(i,j)
+#     ws.cell(row=26, column=3 + i).value = j
+#     data = [ws.cell(row=26, column=i).value for i in range(3, 25)]
+#         # Copying data
+# for cell in data:
+#     ws.cell(row=26+i, column=i).value for i in range(3, 25) = ws.cell(row=26, column=3 + i).value = j
+# # Копіювання даних з рядка С26:Х26
+# for k in range(27, row_count + 1):
+#     # Отримуємо дані з 26-го рядка
+#     data = ws.cell(row=26, column=3).value
+#     # Вставляємо дані в наступний рядок
+#     ws.cell(row=k, column=1).value = data
+
+# print(np.linspace(0.2, 0.5, num=5))
+# print(round(ws['J26'].value,3))
+# print(data)
+# print(range(27, row_count + 1))
+
+# Save the workbook
+
+# # value = np.linspace(0.2,0.5,num = 5)
+# # value = range(0.2, 0.5, 0.05)
+# def make_calculation (value):
+#     last_row = wb['Свод_таблица'].max_row
+#     # cells = ws.getcells(['C26'],['D26'],['E26'],['F26'],['G26'])
+#     # result_page = ws
+#         # value = range(0.2, 0.5, 0.05)
+#     for i in cells:
+#         for j in value:
+#             ws.cell(row=last_row + 1, column=3).value = j
+#         last_row += 1
+# make_calculation (value)
+# print(value)
 
 
-#new_entry = wb['Свод_таблица']['H26':'X26']
-#ws.append(new_entry)
-wb.save('Beetroot_Final_project1.xlsx')
+import openpyxl
+
+# Load the workbook
+# workbook = openpyxl.load_workbook('example.xlsx')
+
+# Select the worksheet
+# worksheet = workbook['Sheet1']
+#
+# # Get the current number of rows
+# num_rows = worksheet.max_row
+#
+# for i in range(10):
+#     # Input the value from the for loop into cells C26:G26
+#     for j in range(5):
+#         worksheet.cell(row=num_rows+1, column=3+j).value = i
+#
+#     # Perform calculations based on formulas embedded in cells H26:X26
+#     # This will update the values in H26:X26
+#     worksheet.calculate_formulas()
+#     workbook.save('example.xlsx')
+#     # Copy the values in row C26:X26 to the next row C27:G27
+#     for col_num in range(3,14):
+#         worksheet.cell(row=num_rows+2, column=col_num).value = worksheet.cell(row=num_rows+1, column=col_num).value
+#     num_rows +=1
+# # save the changes
+# workbook.save('example.xlsx')
+
